000aa689c04a0ddae2c51d8c4de11a06bb04d385
Aswathi Biju <aswathibiju8118@gmail.com>
2025-02-25 21:13:38 +0530

    vorc.c

--- vorc.c
+++ vorc.c
@@ -5,11 +5,13 @@ int main(){
 	printf("Enter a string:\n");
 	scanf("%[^\n]",string);
 	for(i=0;string[i]!='\0';i++){
-		if(string[i]=='a' || string[i]=='e' || string[i]=='i' || string[i]=='o' || string[i]=='u' || string[i]=='A' || string[i]=='E' || string[i]=='I' || string[i]=='O' || string[i]=='U'){
-			v_count++;
-		}else{
-			c_count++;
-		}
+        if ((string[i] >= 'A' && string[i] <= 'Z') || (string[i] >= 'a' && string[i] <= 'z')){
+		    if(string[i]=='a' || string[i]=='e' || string[i]=='i' || string[i]=='o' || string[i]=='u' || string[i]=='A' || string[i]=='E' || string[i]=='I' || string[i]=='O' || string[i]=='U'){
+			    v_count++;
+		    } else{
+                c_count++; 
+		    }
+        }
 	}
 	printf("Vowels : %d \n", v_count);
 	printf("Consonants : %d \n", c_count);
